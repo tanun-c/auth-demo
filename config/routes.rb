606a1ad58773045bc4d822eb63e3b0741cebc89e
Tanun Chalermsinsuwan <tanun.c@gmail.com>
2017-02-04 15:52:51 +0800

    Test page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
 
   get '/protected_information', to: 'protected_information#index'
+  get '/', to: 'test_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
